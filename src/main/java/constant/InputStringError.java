d3de0c48bc9f6c5c7e4cde0cf82cac886aad3e82
SEONG JUN SHIN <shinsj4653@gmail.com>
2024-03-17 18:11:11 +0900

    fix: 입력한 문자열에 대한 에러의 각 경우 주석 추가

--- src/main/java/constant/InputStringError.java
+++ src/main/java/constant/InputStringError.java
@@ -1,7 +1,11 @@
 package constant;
 
 public class InputStringError {
-    public static String wrongInputError = "잘못된 입력입니다.";
-    public static String wrongLengthInputError = "잘못된 길이의 입력입니다.";
-    public static String wrongTypeInputError = "잘못된 타입의 입력입니다.";
+
+    // 게임 진행
+    public static String WRONG_LENGTH_INPUT_ERROR = "잘못된 길이의 입력입니다.";
+    public static String WRONG_TYPE_INPUT_ERROR = "잘못된 타입의 입력입니다.";
+
+    // 게임 종료 후 입력
+    public static String WRONG_INPUT_ERROR = "잘못된 입력입니다.";
 }
