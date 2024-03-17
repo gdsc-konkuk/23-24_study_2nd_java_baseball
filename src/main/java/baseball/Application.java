263c0a290a9d3b807a43ea79a66c725aeca28909
SEONG JUN SHIN <shinsj4653@gmail.com>
2024-03-17 15:49:04 +0900

    feat: 컴퓨터가 3개의 수 선택하는 단계 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+        ArrayList<Integer> computerNums = getComputerNums();
+    }
+
+    private static ArrayList<Integer> getComputerNums() {
+        ArrayList<Integer> computerNums = new ArrayList<>();
+        computerNums.add(Randoms.pickNumberInRange(1, 9));
+        computerNums.add(Randoms.pickNumberInRange(1, 9));
+        computerNums.add(Randoms.pickNumberInRange(1, 9));
+
+        return computerNums;
     }
 }
