c170938f684ea22f555e37981d8666c43c54c54b
hayoon0524 <92996412+hayoon0524@users.noreply.github.com>
2024-03-20 22:34:49 +0900

    컴퓨터 랜덤 숫자 중복 X로 수정

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -10,8 +10,18 @@ public class Computer {
     }
     private void setNums() {
         for (int i = 0; i < 3; i++) {
-            nums[i] = pickNumberInRange(1, 9);
+            int x = pickNumberInRange(1, 9);
+            if (checkNums(x, i)) nums[i] = x;
+            else i--;
         }
+    }
+    private boolean checkNums(int x, int e) {
+        for (int i = 0; i < e; i++) {
+            if (x == nums[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public int[] getNums() {
