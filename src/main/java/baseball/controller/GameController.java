1753e0a2d11346042c0b1bb6a35e530240ae1ebe
GOMUNGNAM <rhrjsgh97@gmail.com>
2024-04-01 20:13:46 +0900

    feat: add necessary fields

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,4 +1,13 @@
 package baseball.controller;
 
+import baseball.model.Player;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class GameController {
+
+	private final InputView inputView;
+	private final OutputView outputView;
+	private Player player;
+
 }
